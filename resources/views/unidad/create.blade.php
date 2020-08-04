a651be798bbbef7f31d3fcaa95d7811e745f07b6
Luis García <lujogarpin78@gmail.com>
2020-08-04 10:07:45 -0500

    modified:   README.md 	modified:   database/seeds/json/tb_areas.json 	modified:   resources/views/unidad/create.blade.php

--- resources/views/unidad/create.blade.php
+++ resources/views/unidad/create.blade.php
@@ -5,8 +5,8 @@
     <div class="form-group">
         <form action="{{url('/unidad/store')}}" class="form-horizontal" method="GET" enctype="multipart/form-data">
             {{ csrf_field() }}
-            <label class="control-label" for="unidad"><b>Unidad</b></label>
-            <input type="text" class="form-control" name="unidad" id="Unidad" value="">
+            <label class="control-label" for="unidadBase"><b>Unidad</b></label>
+            <input type="text" class="form-control" name="unidadBase" id="UnidadBase" value="">
             <br>
             <div>
             <input type="submit" class="btn btn-success" value="Agregar">
