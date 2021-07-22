c80b5c7dac46fa74ce41a675045526a75167c094
lionheartsq <lujogarpin78@gmail.com>
2021-07-22 06:07:29 -0500

    Cambios calculo nomina

--- database/seeds/Tb_nominaSeeder.php
+++ database/seeds/Tb_nominaSeeder.php
@@ -16,7 +16,8 @@ class Tb_nominaSeeder extends Seeder
         DB::table('tb_nomina')->insert([
             'fechaInicio' => '1900-01-01',
             'fechaFin' => '2100-01-01',
-            'tipo' => '0',
+            'tipo' => '1',
+            'observacion' => 'Nómina de prueba',
             'estado' => '1'
         ]);
     }
