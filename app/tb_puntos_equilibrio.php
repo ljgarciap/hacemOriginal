896dbfbe1fa2f4d6e9b8352101907a52dfba18cc
Luis García <lujogarpin78@gmail.com>
2022-08-30 19:01:27 -0500

    Edicion actualizada

--- app/tb_puntos_equilibrio.php
+++ app/tb_puntos_equilibrio.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class tb_puntos_equilibrio extends Model
+class Tb_puntos_equilibrio extends Model
 {
     //
     protected $table = 'tb_puntos_equilibrio';
